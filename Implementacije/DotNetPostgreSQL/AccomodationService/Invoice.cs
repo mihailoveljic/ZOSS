c176c1940b608b369cdaa0292325eee7ea0e85bf
Nemanja Andrić <nandric00@gmail.com>
2024-01-15 21:01:32 +0100

    Add attacks implementation project

---
+++ Implementacije/DotNetPostgreSQL/AccomodationService/Invoice.cs
@@ -0,0 +1,16 @@
+﻿namespace AccomodationService
+{
+	public sealed class Invoice
+	{
+		public Invoice(string name, double price, int quantity)
+		{
+			Name = name;
+			Price = price;
+			Quantity = quantity;
+		}
+
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public int Quantity { get; set; }
+	}
+}
